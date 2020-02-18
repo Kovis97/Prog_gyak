23a9df30fa375d194f19729676e326ff202a8fd0
Kovis97 <galgoczi.aron97@gmail.com>
2020-02-18 11:52:42 +0000

    Szamok negyzete

---
+++ negyzet.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+  cout << "Termeszetets számok negyzetei\n\n";
+  for (int i =1; i<=10;i++)
+    cout << i << " négyzete: " << i*i << '\n';
+  cout << endl;
+  return 0;
+}
