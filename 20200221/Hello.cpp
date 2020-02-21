a3776cb879b62f44a576b87f6de3e52571b13c75
Kovis97 <galgoczi.aron97@gmail.com>
2020-02-21 10:19:26 +0100

    Orai gyakorlas

---
+++ 20200221/Hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Adjon meg ket szamot\n";
+	int a, b;
+	cout << "elso:";
+	cin >> a;
+	cout << "masodik:";
+	cin >> b;
+	cout << "Osszeguk: \t" << a+b << "\nSzorzatuk: \t" << a*b << "\nEzek kulonbsege: \t" << a*b-a+b;
+	return 0;
+}
---
+++ 20200221/Pelda.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello word";
+	cout << "Hello word";
+	cout << "Hello word";
+	cout << "Hello word";
+	cout << "Hello word";
+	cout << "Hello word";
+	cout << "Hello word";
+	cout << "Hello word";
+	return 0;
+}
