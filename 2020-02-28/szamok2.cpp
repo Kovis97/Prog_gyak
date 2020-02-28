0d35afb8890f43033f9b2d1c9cf51c9c1f142ea0
Kovis97 <galgoczi.aron97gmail.com>
2020-02-28 08:47:25 +0000

    Gyakorlás anyaga

---
+++ 2020-02-28/szamok2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int num, parosdb =0, osszdb = 0;
+  cout << "Kérem a számot: ";
+  cin >> num;
+  while (num != -1) {
+    if (num  % 2 == 0) 
+    cout << "Páros\n" << "A fele: " << num/2 << " Ezek darab száma: " << ++parosdb << "\n";
+    else cout << "Páratlan\n";
+    cout << "[Eddigi számok: " << ++osszdb << "\n";
+    cout << "Kérem a következőt: ";
+    cin >> num;
+  }
+  cout <<"\n" <<  osszdb << " szám érkezett, ebből " << parosdb << " páros\n";
+  cout <<"Program vége\n";
+  return 0;
+}
---
+++ 2020-02-28/szamok3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  cout << "Kérem adjon meg számokat.\nKét egymás után azonos szám esetén kilépünk!\n";
+  int akt=0, elozo;
+  do {
+    elozo=akt;
+    cout << "Kérek egy számot: ";
+    cin >> akt;
+  } while (elozo != akt);
+  return 0;
+}
