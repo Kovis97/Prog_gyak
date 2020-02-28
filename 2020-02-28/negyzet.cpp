71b9ca0a7802dccaff090ecd6db4c1a0d8e982fa
Kovis97 <galgoczi.aron97@gmail.com>
2020-02-28 22:20:39 +0100

    folytatás

--- 2020-02-28/negyzet.cpp
+++ 2020-02-28/negyzet.cpp
@@ -1,7 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-  
+  int negyzet;
+  cout << "Kérem adja meg a négyzet oldalának hosszát!: ";
+  cin >> negyzet;
+  int sor = -negyzet;
+  while (sor <= negyzet) {
+    int oszlop = -negyzet;
+    while (oszlop <= negyzet) {
+      if (negyzet*negyzet >= sor*sor + oszlop*oszlop) 
+        cout << '*';
+      else
+        cout << ' ';
+      oszlop++;      
+    }
+  sor += 2;
+  cout << '\n';
+  }
   return 0;
 }
