86974cd04090679eb08fe71b7ec30531a3fedd16
Kovis97 <galgoczi.aron97@gmail.com>
2020-04-21 16:14:12 +0200

    Repl.it konfigurálása

---
+++ setapos.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#repl.it-in lévő git configurálása!
+# echo "sajt"
+echo ""
+cd Prog_gyak/
+chmod +xr runs.sh
+ls -a
+pwd;
+git config --global user.email "galgoczi.aron97@gmail.com"
+git config --global user.name "Kovis97"
+git status
+git pull origin master
+
