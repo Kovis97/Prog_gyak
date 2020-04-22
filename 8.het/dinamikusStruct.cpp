227a59f6f8f7c7b36a9f5e9b32ba702589419fde
Kovis97 <galgoczi.aron97@gmail.com>
2020-04-22 18:09:12 +0200

    8. het 3. feladat dinamikus structura

--- 8.het/dinamikusStruct.cpp
+++ 8.het/dinamikusStruct.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 struct koord {
     int x,y;
 };
 
+/*
 void beolvas(koord *pontok){
     cout << "X: ";cin >> pontok->x;
     cout << "Y: ";cin >> pontok->y;
@@ -12,8 +14,30 @@ void beolvas(koord *pontok){
 
 int main() {
     koord *pontok = new koord;
+    if (pontok==NULL) {
+        cerr << "Memoriafoglalasi hiba."<< endl;
+        return 1;
+    }
+    
     cout << "Ajdjon meg két koordinatat\n";
-    beolvas(pontok);
+    beolvas(pontok); */
+
+koord* beolvas(){
+    koord* pontok = new koord;
+    if (pontok==NULL) {
+        cerr << "Memoriafoglalasi hiba."<< endl;
+        exit(1);
+    }
+    cout << "X: ";cin >> pontok->x;
+    cout << "Y: ";cin >> pontok->y;
+    return pontok;
+}
+
+int main() {
+    koord* pontok;    
+    cout << "Adjon meg két koordinatat\n";
+    pontok = beolvas();
+    cout << "A beolvasott koordinatak\n";
     cout << "X: "<< pontok->x << endl;
     cout << "Y: "<< pontok->y << endl;
     delete pontok;
