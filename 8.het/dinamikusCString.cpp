71207cbf9201a6681cc103f3bd406d7a20994506
Kovis97 <galgoczi.aron97@gmail.com>
2020-04-27 18:54:03 +0200

    8.het 4.feladat

---
+++ 8.het/dinamikusCString.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define DB 100
+
+int main() {
+  //char szoveg[DB+1];
+  string szoveges;
+  char *mszoveg;
+  cout << "Adjon meg egy karakter láncot!\n";
+  cin >> szoveges;
+  /*mszoveg = new char[strlen(szoveg)+1];
+  strcpy(mszoveg,szoveg);
+  cout << "a szoveg: "<< szoveg << endl; */
+  if (!(szoveges.length<DB))
+  {
+    cout << "A szöveg tul hosszu!";
+    return 0;
+  }
+  
+  mszoveg = new char[szoveges.length()+1];
+  if (mszoveg==NULL)
+  {
+    cerr << "Memória foglalás sikerten!\n";
+    return -1;
+  }
+  
+  strcpy(mszoveg,szoveges.c_str);
+  cout << "a szoveg: "<< szoveges << endl;
+
+  return 0;
+}
