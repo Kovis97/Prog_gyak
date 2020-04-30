02661f0086a34663356de1bafa2a6e58caff75a5
Kovis97 <galgoczi.aron97@gmail.com>
2020-04-30 13:57:19 +0000

    formazva

--- 10.het/datum.cpp
+++ 10.het/datum.cpp
@@ -1,5 +1,33 @@
 #include "datum.h"
 
-void beolvas(datum*) {
-    
+void beolvas(datum *date)
+{
+    cout << "Adja meg az evet: ";
+    cin >> date->ev;
+    cout << "Adja meg a honapot: ";
+    cin >> date->ho;
+    cout << "Adja meg az napot: ";
+    cin >> date->nap;
+}
+
+void kiir(datum *date)
+{
+    cout << date->ev << "." << date->ho << "." << date->nap;
+}
+
+int osszehasonlit(const datum *date1, const datum *date2)
+{
+    if (date1->ev != date2->ev)
+    {
+        return date1->ev - date2->ev;
+    }
+    else
+    {
+        if (date1->ho != date2->ho)
+        {
+            return date1->ho - date2->ho;
+        }
+        else
+            return date1->nap - date2->nap;
+    }
 }
