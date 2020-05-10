9cab87c5ffe8a6a8e01202d8580cdaff41f67c58
Kovis97 <galgoczi.aron97@gmail.com>
2020-05-10 16:18:15 +0200

    13. heti 2. feladattal modositva

--- 13.het/ujFile.cpp
+++ 13.het/ujFile.cpp
@@ -13,6 +13,19 @@ int main() {
         f << "Juppi!\n";
         f << "Hello Word!\n";
         f.close();
-    }    
+    } else {
+        cerr << "Hiba a fájl létrehozasakor!";
+        return 0;
+    }
+    ifstream fo(NEV);
+    if (fo.is_open()) {
+        string sor;
+        while(getline(fo, sor)) {
+            cout << sor << endl;
+        }
+        fo.close();
+    } else {
+        cerr << "Hiba a fajl olvasasakor!";
+    }
     return 0;
 }
