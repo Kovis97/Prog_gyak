c211a088c4e70400a22cbe727ad3885fd29a84af
Kovis97 <galgoczi.aron97@gmail.com>
2020-05-12 16:05:08 +0200

    13.heti 4.feladat

---
+++ 13.het/fajlBinaris.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+streampos meretszamit(ifstream &f)
+{
+    streampos akt, meret;
+    akt = f.tellg();
+    f.seekg(0, ios_base::end);
+    meret = f.tellg();
+    f.seekg(akt, ios_base::beg);
+    return meret;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        cout << "A program helyes használata:\n\t" << argv[0]
+             << " fajl_azonosito\n";
+        return 1;
+    }
+    ifstream f(argv[1], ios_base::binary);
+    if (not f.is_open())
+    {
+        cout << argv[1] << "nem nyithato meg!\n";
+        return 2;
+    }
+    streampos meret = meretszamit(f);
+    int i = 0;
+    char c;
+    while (f.get(c))
+    {
+        i++;
+        if (c<' ' and c!='\n' and c!='\t' and c!='\r' and (c!=26 or i!=meret))
+        {
+            cout << "A(z) " << argv[1] << " valoszinuleg binaris\n";
+            f.close();
+            return 0;
+        }
+        
+    }
+    cout << "A(z) " << argv[1] << " valoszinuleg szoveges\n";
+    f.close();
+    return 0;
+}
